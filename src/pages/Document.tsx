ff3d9094e0f170be8c138f2b6b21b0a81b5f630b
Marty Schoch <marty.schoch@gmail.com>
2024-12-02 15:55:30 -0500

    updated to properly check the actuallyReady flag

--- src/pages/Document.tsx
+++ src/pages/Document.tsx
@@ -12,32 +12,26 @@ export function Document() {
     const { name, docID } = useParams()
     const { database} = useFireproof(name)
     const [docContent, setDocContent] = useState('');
-
     const [actuallyReady, setActuallyReady] = useState(false);
-
 
     const cx = connect(database, '', 'https://' + window.location.hostname);
     cx.loader?.ready().then(value => {
+        console.log('cx loader ready');
         cx.loader?.remoteMetaStore?.load().then(value1 => {
+            console.log('remote meta store loaded');
             setActuallyReady(true);
             console.log("connected", cx);
         })
     })
 
-    // cx.loaded.then(value => {
-    //     window.setTimeout(args => {
-    //         setActuallyReady(true);
-    //         console.log("connected", cx);
-    //     }, 5000)
-    // })
-
-
     useEffect(() => {
-        const fetchItem = async () => {
-            const doc = (await database.get(docID!)) as DatabaseDoc
-            setDocContent(JSON.stringify(doc, null, 4))
+        if (actuallyReady) {
+            const fetchItem = async () => {
+                const doc = (await database.get(docID!)) as DatabaseDoc
+                setDocContent(JSON.stringify(doc, null, 4))
+            }
+            fetchItem()
         }
-        fetchItem()
     }, [actuallyReady])
 
     return (
